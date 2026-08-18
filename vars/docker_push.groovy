cc3950d464f029daece0ab33a382988f5fe84fe9
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-08-18 15:15:22 +0530

    Refactor docker_push to accept parameters and update login

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,19 +1,25 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def credentials = config.credentials ?: 'docker-hub-credentials'
-    
-    echo "Pushing Docker image: ${imageName}:${imageTag}"
-    
-    withCredentials([usernamePassword(
-        credentialsId: credentials,
-        usernameVariable: 'DOCKER_USERNAME',
-        passwordVariable: 'DOCKER_PASSWORD'
-    )]) {
+def call(String Project, String ImageTag, String dockerhubuser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'DockerHubCred',
+            usernameVariable: 'dockerHubUser',
+            passwordVariable: 'dockerHubPass'
+        )
+    ]) {
+
         sh """
-            echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin
-            docker push ${imageName}:${imageTag}
-            docker push ${imageName}:latest
+            echo "\$dockerHubPass" | docker login \
+                -u "\$dockerHubUser" \
+                --password-stdin
+
+            docker tag ${Project}:latest \
+                ${dockerhubuser}/${Project}:${ImageTag}
+
+            docker push \
+                ${dockerhubuser}/${Project}:${ImageTag}
+
+            docker logout
         """
     }
 }
