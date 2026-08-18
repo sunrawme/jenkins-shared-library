b3038b04ba145a88a51135b485d848f1c47fccb9
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-08-18 14:57:36 +0530

    Refactor docker_build function for simpler usage

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,3 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
-    sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
-    """
+def call(String projectName, String ImageTag, String DockerHubuser){
+   sh "docker build -t ${DockerHubeuser}/${projectName}:${ImageTag} ."
 }
